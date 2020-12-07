6fd059c03b991645d4d7d2afefb603c909b6ab81
Surefirer <46170182+Surefirer@users.noreply.github.com>
2020-12-07 13:17:01 -0800

    delete log

--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -1,4 +1,10 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from '../action/types';
+import {
+  GET_LOGS,
+  SET_LOADING,
+  LOGS_ERROR,
+  ADD_LOG,
+  DELETE_LOG,
+} from '../action/types';
 
 const initialState = {
   logs: null,
@@ -21,6 +27,12 @@ export default (state = initialState, action) => {
       return {
         ...state,
         logs: [...state, action.payload],
+        loading: false,
+      };
+    case DELETE_LOG:
+      return {
+        ...state,
+        logs: state.logs.filter((log) => log.id !== action.payload),
         loading: false,
       };
     case SET_LOADING:
